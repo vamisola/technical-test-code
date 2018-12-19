b85bb18e8b1454cd72b3794e64642be4d9eac9c9
Vina Compson <vv.compson@icloud.com>
2018-12-19 23:32:22 +1100

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+//prod.js - prod keys
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+
+mongodb://technical-tester:GuDLJQiQmgz8HtqEUHH}@ds259111.mlab.com:59111/technical-test-prod
